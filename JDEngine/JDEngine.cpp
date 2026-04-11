8141cc914771492ba964561d690ef17a6b3381d0
Joe Arthur Maltby Derrett <63147552+puffledude@users.noreply.github.com>
2026-04-11 17:13:50 +0100

    Add project files.

---
+++ JDEngine/JDEngine.cpp
@@ -0,0 +1,12 @@
+﻿// JDEngine.cpp : Defines the entry point for the application.
+//
+
+#include "JDEngine.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
