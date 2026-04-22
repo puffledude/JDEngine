05176d87bb3eb758550e0afa7214653267625289
JDerrett02 <J.A.M.Derrett2@ncl.ac.uk>
2026-04-22 16:25:50 +0100

    Make things work better

--- JDEngine/GameWorld.cpp
+++ JDEngine/GameWorld.cpp
@@ -13,17 +13,19 @@ namespace JD
 	{
 		std::vector<RenderTransmition>* renderTransmition = new std::vector<RenderTransmition>();
 		auto view = registry->view<RenderableComponent, JoltComponent>();
-		for (auto entity : view) {
-			auto [renderable, jolt] = view.get<RenderableComponent, JoltComponent>(entity);
-			for (auto& mesh : *renderable.mesh) {
 
+		// Use .each() to smoothly unpack components by reference
+		for (auto [entity, renderable, jolt] : view.each()) {
+
+			for (auto& mesh : *renderable.mesh) {
 				/*RenderTransmition transmition;
 				transmition.mesh = renderable.mesh[0];
 				transmition.uboIndex = jolt.bodyID.GetIndex();
 				renderTrans*/
 			}
-			return renderTransmition;
 		}
 
+		// Move the return statement OUTSIDE of the for-loop!
+		return renderTransmition;
 	}
 }
